8747f8ed1f95ec665b82125ead4699ca3594ae83
Dino Costantini <dino.costantini@gmail.com>
2021-03-24 18:34:53 +0000

    Update build script

--- build_rasp.ps1
+++ build_rasp.ps1
@@ -13,6 +13,10 @@ else
     write-host "mkdir rasp-target already exists"
 }
 
-scp bitnami@192.168.1.186:~/graphql_cache/target/arm-unknown-linux-musleabi/release/graphql_cache rasp-target/
-scp rasp-target/graphql_cache pi@192.168.1.50:~/graphql_cache
-ssh pi@192.168.1.50 "chmod a+x graphql_cache"
+$BRANCH=(git rev-parse --abbrev-ref HEAD)
+$FILE_NAME="graphql_cache_" + $BRANCH
+$PATH="rasp-target/graphql_cache_" + $BRANCH
+
+scp bitnami@192.168.1.186:~/graphql_cache/target/arm-unknown-linux-musleabi/release/graphql_cache $PATH
+scp $PATH pi@192.168.1.50:~/$FILE_NAME
+ssh pi@192.168.1.50 "chmod a+x "$FILE_NAME
