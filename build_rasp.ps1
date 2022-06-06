85e72cd7bdbb2ed73cf8cb954d5d04e222a34921
Dino Costantini <dino.costantini@gmail.com>
2022-06-06 18:19:18 +0100

    Update build script and rust edition

--- build_rasp.ps1
+++ build_rasp.ps1
@@ -1,8 +1,8 @@
 tar -czf graphql_cache.tar.gz src Cargo.toml
-scp build.sh bitnami@192.168.1.186:~/
-scp graphql_cache.tar.gz bitnami@192.168.1.186:~/
+scp build.sh bitnami@192.168.137.128:~/
+scp graphql_cache.tar.gz bitnami@192.168.137.128:~/
 
-ssh bitnami@192.168.1.186 "chmod a+x build.sh;./build.sh"
+ssh bitnami@192.168.137.128 "chmod a+x build.sh;./build.sh"
 
 if (!(Test-Path "rasp-target"))
 {
@@ -17,7 +17,7 @@ $BRANCH=(git rev-parse --abbrev-ref HEAD)
 $FILE_NAME="graphql_cache_" + $BRANCH
 $PATH="rasp-target/graphql_cache_" + $BRANCH
 
-scp bitnami@192.168.1.186:~/graphql_cache/target/arm-unknown-linux-musleabi/release/graphql_cache $PATH
+scp bitnami@192.168.137.128:~/graphql_cache/target/arm-unknown-linux-musleabi/release/graphql_cache $PATH
 ssh pi@192.168.1.50 "killall ${FILE_NAME}; killall ./${FILE_NAME}"
 scp $PATH pi@192.168.1.50:~/$FILE_NAME
 ssh pi@192.168.1.50 "chmod a+x ${FILE_NAME} && ./${FILE_NAME}"
